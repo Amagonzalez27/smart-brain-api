aebb612c997668b885bab005a39c5191caf6a10e
Amanda G <amagonzalez27@gmail.com>
2018-07-17 16:08:36 -0400

    adding starting project

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  bcrypt = require("bcrypt-nodejs"),
+  cors = require("cors"),
+  knex = require("knex"),
+  register = require("./controllers/register"),
+  signin = require("./controllers/signin"),
+  profile = require("./controllers/profile"),
+  image = require("./controllers/image");
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1", //localhost
+    user: "amanda",
+    password: "",
+    database: "smart-brain"
+  }
+});
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send(database.users);
+});
+app.post("/signin", signin.handleSignin(db, bcrypt));
+app.post("/register", register.handleRegister(db, bcrypt));
+app.get("/profile/:id", profile.handleProfileGet(db));
+app.put("/image", image.handleImage(db));
+app.post("/imageurl", image.handleApiCall());
+app.listen(3000, () => {
+  console.log("APP IS RUNNING ON PORT 3000");
+});
